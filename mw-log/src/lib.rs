095fae9af3d9872aac15a94f1f29ad19817ac2a3
tiannian <dtiannian@gmail.com>
2020-08-24 17:19:38 +0800

    basic init for log

--- mw-log/src/lib.rs
+++ mw-log/src/lib.rs
@@ -1,7 +1,32 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![no_std]
+#![feature(fmt_as_str)]
+
+use log::{Record, Level, Metadata, SetLoggerError, LevelFilter};
+
+struct MWLogger;
+
+impl log::Log for MWLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Info
     }
+
+    fn log(&self, record: &Record) {
+        extern {
+            fn println_stdout(s: *const u8);
+        }
+        if self.enabled(record.metadata()) {
+            let out_str = record.args().as_str().unwrap();
+            unsafe { println_stdout(out_str.as_ptr()) };
+            // println!("{} - {}", record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+static LOGGER: MWLogger = MWLogger;
+
+pub fn init() -> Result<(), SetLoggerError> {
+    log::set_logger(&LOGGER)
+        .map(|()| log::set_max_level(LevelFilter::Info))
 }
