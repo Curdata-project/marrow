b59161f5d847731d1d51dd3efb9c395c3889a424
Baoyuan <baoyuan.top@gmail.com>
2020-12-21 17:46:43 +0800

    code clean

--- runtime/sqlite/index.ts
+++ runtime/sqlite/index.ts
@@ -7,10 +7,8 @@ const db = new sqlite.Database("test.db");
 
 export const _sql_run_callback = (ptr: number, path_length: number, fn: any, addr: any) => {
   const sql = getValue(ptr, path_length);
-  console.log(sql, "sql");
   db.run(sql, (err) => {
     if (err) {
-      console.log(err, "run err");
       const { ptr, length } = setValue("fail");
       wasm_exports.call_sql_callback_fn(ptr, length, fn, addr);
     } else {
@@ -22,14 +20,11 @@ export const _sql_run_callback = (ptr: number, path_length: number, fn: any, add
 
 export const _sql_query_callback = (ptr: number, path_length: number, fn: any, addr: any) => {
   const sql = getValue(ptr, path_length);
-  console.log(sql, "sql");
   db.all(sql, (err, data) => {
     if (err) {
-      console.log(err, "query err")
       const { ptr, length } = setValue("fail");
       wasm_exports.call_sql_callback_fn(ptr, length, fn, addr);
     } else {
-      console.log(data, "query data")
       const { ptr, length } = setValue(JSON.stringify(data));
       wasm_exports.call_sql_callback_fn(ptr, length, fn, addr);
     }
