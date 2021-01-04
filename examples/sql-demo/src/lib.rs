cc293049049ddc97586116146c10a373de9a35ec
malefooo <784479660@qq.com>
2021-01-04 15:10:22 +0800

    test:add debug msg

--- examples/sql-demo/src/lib.rs
+++ examples/sql-demo/src/lib.rs
@@ -14,21 +14,19 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[mw_rt::async_main]
 async fn main() {
+    mw_std::debug::println("sql test start");
     let mut message = proto::sql_demo::Sql::default();
     let str = "select * from test_db from id = $1";
     message.sql = Cow::Borrowed(str);
     let param = String::from("123456qwer7890").as_bytes().to_vec();
     message.params.push(Cow::Owned(param));
     let mut out: Vec<u8> = Vec::new();
-
+    mw_std::debug::println(&alloc::format!("message:{:?}", message));
     let serialize_result = quick_protobuf::serialize_into_slice(&message, out.as_mut_slice());
     if serialize_result.as_ref().is_err() {
+        mw_std::debug::println("serialize err");
         return;
     }
-    sql_fun(out.as_slice()).await;
-}
-
-async fn sql_fun(bytes: &[u8]) {
-    let result = mw_std::sql::sql_execute(bytes, 0);
+    let result = mw_std::sql::sql_execute(out.as_slice(), 0).await;
     mw_std::debug::println(&alloc::format!("{:?}", result));
 }
--- examples/sql-demo/src/proto/sql_demo.rs
+++ examples/sql-demo/src/proto/sql_demo.rs
@@ -1,9 +1,8 @@
-
-use alloc::vec::Vec;
-use alloc::borrow::Cow;
-use quick_protobuf::{MessageRead, MessageWrite, BytesReader, Writer, WriterBackend, Result};
-use quick_protobuf::sizeofs::*;
 use super::*;
+use alloc::borrow::Cow;
+use alloc::vec::Vec;
+use quick_protobuf::sizeofs::*;
+use quick_protobuf::{BytesReader, MessageRead, MessageWrite, Result, Writer, WriterBackend};
 
 #[derive(Debug, Default, PartialEq, Clone)]
 pub struct Sql<'a> {
@@ -18,7 +17,9 @@ impl<'a> MessageRead<'a> for Sql<'a> {
             match r.next_tag(bytes) {
                 Ok(10) => msg.sql = r.read_string(bytes).map(Cow::Borrowed)?,
                 Ok(18) => msg.params.push(r.read_bytes(bytes).map(Cow::Borrowed)?),
-                Ok(t) => { r.read_unknown(bytes, t)?; }
+                Ok(t) => {
+                    r.read_unknown(bytes, t)?;
+                }
                 Err(e) => return Err(e),
             }
         }
@@ -28,15 +29,24 @@ impl<'a> MessageRead<'a> for Sql<'a> {
 
 impl<'a> MessageWrite for Sql<'a> {
     fn get_size(&self) -> usize {
-        0
-        + if self.sql == "" { 0 } else { 1 + sizeof_len((&self.sql).len()) }
-        + self.params.iter().map(|s| 1 + sizeof_len((s).len())).sum::<usize>()
+        0 + if self.sql == "" {
+            0
+        } else {
+            1 + sizeof_len((&self.sql).len())
+        } + self
+            .params
+            .iter()
+            .map(|s| 1 + sizeof_len((s).len()))
+            .sum::<usize>()
     }
 
     fn write_message<W: WriterBackend>(&self, w: &mut Writer<W>) -> Result<()> {
-        if self.sql != "" { w.write_with_tag(10, |w| w.write_string(&**&self.sql))?; }
-        for s in &self.params { w.write_with_tag(18, |w| w.write_bytes(&**s))?; }
+        if self.sql != "" {
+            w.write_with_tag(10, |w| w.write_string(&**&self.sql))?;
+        }
+        for s in &self.params {
+            w.write_with_tag(18, |w| w.write_bytes(&**s))?;
+        }
         Ok(())
     }
 }
-
