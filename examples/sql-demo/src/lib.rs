de106ddd7384f8bf9671f4abcc419474f07073bb
malefooo <784479660@qq.com>
2021-01-04 17:28:57 +0800

    test:add sql-demo test

--- examples/sql-demo/src/lib.rs
+++ examples/sql-demo/src/lib.rs
@@ -3,10 +3,11 @@
 
 extern crate alloc;
 
-use alloc::borrow::Cow;
-use alloc::string::String;
-use alloc::vec::Vec;
-
+use alloc::borrow::Cow; 
+use alloc::string::String; 
+use alloc::vec::Vec; 
+use alloc::vec;
+use quick_protobuf::{MessageWrite, sizeofs};
 mod proto;
 
 #[global_allocator]
@@ -14,19 +15,19 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[mw_rt::async_main]
 async fn main() {
-    mw_std::debug::println("sql test start");
-    let mut message = proto::sql_demo::Sql::default();
-    let str = "select * from test_db from id = $1";
-    message.sql = Cow::Borrowed(str);
-    let param = String::from("123456qwer7890").as_bytes().to_vec();
-    message.params.push(Cow::Owned(param));
-    let mut out: Vec<u8> = Vec::new();
-    mw_std::debug::println(&alloc::format!("message:{:?}", message));
-    let serialize_result = quick_protobuf::serialize_into_slice(&message, out.as_mut_slice());
-    if serialize_result.as_ref().is_err() {
-        mw_std::debug::println("serialize err");
-        return;
-    }
-    let result = mw_std::sql::sql_execute(out.as_slice(), 0).await;
-    mw_std::debug::println(&alloc::format!("{:?}", result));
+    let mut message = proto::sql_demo::Sql::default();     
+    let str = "select * from test_db from id = $1";     
+    message.sql = Cow::Borrowed(str);     
+    let param = String::from("123456qwer7890").as_bytes().to_vec();     
+    message.params.push(Cow::Owned(param));     
+
+    let mut out: Vec<u8> = vec![0;sizeofs::sizeof_len(message.get_size())];     
+    let serialize_result = quick_protobuf::serialize_into_slice(&message, out.as_mut_slice()); 
+    if serialize_result.as_ref().is_err() {         
+        let e = serialize_result.err();         
+        mw_std::debug::println(&alloc::format!("err:{:?}", e));         
+        mw_std::debug::println(&alloc::format!("vec:{:?}", out));         
+        return;     
+    };
+    mw_std::sql::sql_execute(out.as_slice(), 0).await;
 }
